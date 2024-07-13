384d5cf195d6a37d3fe48a03d36c6e826b680bd0
Lexe <ngocle.ng1001@gmail.com>
2024-07-14 00:38:20 +0700

    refactor: book card component

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -4,35 +4,32 @@ import PropTypes from "prop-types";
 import "./BookCard.scss";
 
 const BookCard = ({ data }) => {
-  console.log(data);
   return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url("${data.imageLinks.thumbnail}")`
-            }}
-          ></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="none" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+    <div className="book">
+      <div className="book__top">
+        <div
+          className="book__cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url("${data.imageLinks.thumbnail}")`
+          }}
+        ></div>
+        <div className="book__shelf-changer">
+          <select>
+            <option value="none" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{data.title}</div>
-        <div className="book-authors">{data.authors.join(", ")}</div>
       </div>
-    </li>
+      <div className="book__title">{data.title}</div>
+      <div className="book__authors">{data.authors.join(", ")}</div>
+    </div>
   );
 };
 
